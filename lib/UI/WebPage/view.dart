882e726f5738952685fec2c106151d3e7e33f716
Your Name <you@example.com>
2024-02-20 20:29:04 +0100

    new

--- lib/UI/WebPage/view.dart
+++ lib/UI/WebPage/view.dart
@@ -23,21 +23,25 @@ class WebPageView extends StatelessWidget {
               child: Stack(
             children: [
               InAppWebView(
-                initialUrlRequest:
-                    URLRequest(url: WebUri(model.link.toString())),
-                initialOptions: InAppWebViewGroupOptions(
-                    crossPlatform: InAppWebViewOptions(
-                      transparentBackground: true,
-                      mediaPlaybackRequiresUserGesture: false,
+                initialUrlRequest: URLRequest(url: WebUri(model.link.toString())),
+                initialSettings: InAppWebViewSettings(
+                  allowContentAccess: true,
+                  allowsBackForwardNavigationGestures: true,
+                  mediaPlaybackRequiresUserGesture: false,
+                  allowsInlineMediaPlayback: true
+                    // crossPlatform: InAppWebViewSettings(
+                    //   transparentBackground: true,
+                    //   mediaPlaybackRequiresUserGesture: false,
+                    // ),
+                    // ios: InAppWebViewSettings(
+                    //   allowsInlineMediaPlayback: true,
+                    // )
+
                     ),
-                    ios: IOSInAppWebViewOptions(
-                      allowsInlineMediaPlayback: true,
-                    )),
-                androidOnPermissionRequest: (InAppWebViewController controller,
-                    String origin, List<String> resources) async {
-                  return PermissionRequestResponse(
-                      resources: resources,
-                      action: PermissionRequestResponseAction.GRANT);
+                onPermissionRequest: (controller, request) async {
+                  return PermissionResponse(
+                      resources: request.resources,
+                      action: PermissionResponseAction.GRANT);
                 },
                 onLoadStart: (InAppWebViewController controller, Uri? url) {
                   if (url != null) {
@@ -73,3 +77,98 @@ class WebPageView extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =============== old codeeeeeeeeeeeeeeeeee 
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:gomobilez/UI/WebPage/viewModel.dart';
+// import 'package:gomobilez/app/app.router.dart';
+// import 'package:gomobilez/helpers/app_colors.dart';
+// import 'package:gomobilez/widgets/roundedIconButton.dart';
+// import 'package:stacked/stacked.dart';
+
+// class WebPageView extends StatelessWidget {
+//   final String url;
+//   const WebPageView({Key? key, required this.url}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final args =
+//         ModalRoute.of(context)!.settings.arguments as WebPageViewArguments;
+//     return ViewModelBuilder<WebPageViewModel>.reactive(
+//       onViewModelReady: (model) => model.init(url: args.url),
+//       viewModelBuilder: () => WebPageViewModel(),
+//       builder: (context, model, child) => Scaffold(
+//           backgroundColor: primaryColor,
+//           body: SafeArea(
+//               child: Stack(
+//             children: [
+//               InAppWebView(
+//                 initialUrlRequest:
+//                     URLRequest(url: WebUri(model.link.toString())),
+//                 initialOptions: InAppWebViewGroupOptions(
+//                     crossPlatform: InAppWebViewOptions(
+//                       transparentBackground: true,
+//                       mediaPlaybackRequiresUserGesture: false,
+//                     ),
+//                     ios: IOSInAppWebViewOptions(
+//                       allowsInlineMediaPlayback: true,
+//                     )),
+//                 androidOnPermissionRequest: (InAppWebViewController controller,
+//                     String origin, List<String> resources) async {
+//                   return PermissionRequestResponse(
+//                       resources: resources,
+//                       action: PermissionRequestResponseAction.GRANT);
+//                 },
+//                 onLoadStart: (InAppWebViewController controller, Uri? url) {
+//                   if (url != null) {
+//                     if (url.path == '/home') {
+//                       NavigationActionPolicy.CANCEL;
+//                       return model.pop();
+//                     }
+//                   }
+//                 },
+//                 shouldOverrideUrlLoading: (controller, navigationAction) async {
+//                   var uri = navigationAction.request.url!;
+
+//                   print(uri.path);
+
+//                   if (uri.path == '/home') {
+//                     NavigationActionPolicy.CANCEL;
+//                     return model.pop();
+//                   }
+
+//                   return NavigationActionPolicy.CANCEL;
+//                 },
+//               ),
+//               Padding(
+//                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+//                 child: RoundedIconButton(
+//                     color: transparentWhite,
+//                     padding: 5,
+//                     click: () => model.pop(),
+//                     icon: Icon(Icons.arrow_back)),
+//               )
+//             ],
+//           ))),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:bot_toast/bot_toast.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:gomobilez/firebase_options.dart';
 import 'package:gomobilez/helpers/app_colors.dart';
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:gomobilez/services/firebaseService.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -63,3 +60,86 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:gomobilez/UI/WebPage/viewModel.dart';
+// import 'package:gomobilez/app/app.router.dart';
+// import 'package:gomobilez/helpers/app_colors.dart';
+// import 'package:gomobilez/widgets/roundedIconButton.dart';
+// import 'package:stacked/stacked.dart';
+
+// class WebPageView extends StatelessWidget {
+//   final String url;
+//   const WebPageView({Key? key, required this.url}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final args =
+//         ModalRoute.of(context)!.settings.arguments as WebPageViewArguments;
+//     return ViewModelBuilder<WebPageViewModel>.reactive(
+//       onViewModelReady: (model) => model.init(url: args.url),
+//       viewModelBuilder: () => WebPageViewModel(),
+//       builder: (context, model, child) => Scaffold(
+//           backgroundColor: primaryColor,
+//           body: SafeArea(
+//               child: Stack(
+//             children: [
+//               InAppWebView(
+//                 initialUrlRequest: URLRequest(url: WebUri(model.link.toString())),
+//                 initialSettings: InAppWebViewSettings(
+//                   allowContentAccess: true,
+//                   allowsBackForwardNavigationGestures: true,
+//                   mediaPlaybackRequiresUserGesture: false,
+//                   allowsInlineMediaPlayback: true
+//                     // crossPlatform: InAppWebViewSettings(
+//                     //   transparentBackground: true,
+//                     //   mediaPlaybackRequiresUserGesture: false,
+//                     // ),
+//                     // ios: InAppWebViewSettings(
+//                     //   allowsInlineMediaPlayback: true,
+//                     // )
+
+//                     ),
+//                 onPermissionRequest: (controller, request) async {
+//                   return PermissionResponse(
+//                       resources: request.resources,
+//                       action: PermissionResponseAction.GRANT);
+//                 },
+//                 onLoadStart: (InAppWebViewController controller, Uri? url) {
+//                   if (url != null) {
+//                     if (url.path == '/home') {
+//                       NavigationActionPolicy.CANCEL;
+//                       return model.pop();
+//                     }
+//                   }
+//                 },
+//                 shouldOverrideUrlLoading: (controller, navigationAction) async {
+//                   var uri = navigationAction.request.url!;
+
+//                   print(uri.path);
+
+//                   if (uri.path == '/home') {
+//                     NavigationActionPolicy.CANCEL;
+//                     return model.pop();
+//                   }
+
+//                   return NavigationActionPolicy.CANCEL;
+//                 },
+//               ),
+//               Padding(
+//                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+//                 child: RoundedIconButton(
+//                     color: transparentWhite,
+//                     padding: 5,
+//                     click: () => model.pop(),
+//                     icon: Icon(Icons.arrow_back)),
+//               )
+//             ],
+//           ))),
+//     );
+//   }
+// }
